8ae68de873002e1667f7f5209831eb217b622968
JorgeDirkx <jorge.dirkx@gmail.com>
2020-06-21 21:55:03 +0200

    review: int result

--- src/main/java/com/jorgedirkx/ConnectDB.java
+++ src/main/java/com/jorgedirkx/ConnectDB.java
@@ -1,33 +1,44 @@
 package com.jorgedirkx;
 
 import java.lang.module.ResolutionException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ConnectDB {
     public static void main(String[] args) {
 
         String sql = "SELECT * FROM project";
 
+        String sqlUpdate = "UPDATE project SET name='to be determined' WHERE Id =1";
+
+        String sqlAddData = "INSERT INTO project(name)" + "VALUES('Project 2')";
+
         //testing the thorius_db connection with JDBC
         try(Connection conn = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/thorius_db?serverTimezone=UTC","root","Doreen1");
 
+            Statement statement = conn.createStatement()){
+            int result = statement.executeUpdate(sqlUpdate);
+            int result2 = statement.executeUpdate(sqlAddData);
+
+            /*
             //resultset can be read all directions and resultset can be updated
             Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
 
             //request data from db
-            ResultSet rs = statement.executeQuery(sql)){
+            ResultSet rs = statement.executeQuery(sql)) {
 
             while (rs.next()){
                 String name = rs.getString("name");
                 System.out.format("%s\n", name);
 
                 rs.close();
-            }
-            System.out.println("connection OK");
+
+
+        }
+
+             */
+
+        System.out.println("connection OK");
 
 
         }
